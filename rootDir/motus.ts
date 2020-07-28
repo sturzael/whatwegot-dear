062440d11e30afd253ec35dec8635f8cc0c37147
Elliot Sturzaker <esturz23@gmail.com>
2020-07-29 09:32:42 +1200

    init typescript

---
+++ rootDir/motus.ts
@@ -0,0 +1,10 @@
+function greeter(person) {
+    return "Hellos my name is, " + person;
+}
+
+
+
+
+var user = "Jane User";
+
+document.body.innerHTML = greeter(user);
---
+++ ts-built/motus.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hellos my name is, " + person;
+}
+var user = "Jane User";
+document.body.innerHTML = greeter(user);
